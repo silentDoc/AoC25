42c3b34e26ec0028518a3afbf2cefc211c0c8c7d
silentDoc <ramon78t@yahoo.es>
2025-12-01 08:02:57 +0100

    Day 1 - Part 1 - Test Case working fine

--- Day01/Solver.cs
+++ Day01/Solver.cs
@@ -1,20 +1,36 @@
-﻿namespace AoC25.Day01
+﻿using AoC25.Common;
+
+namespace AoC25.Day01
 {
     public class Solver
     {
         public static string Solve(List<string> input, int part)
-        {
-            return part switch
+            => part switch
             {
                 1 => SolvePart1(input),
                 2 => SolvePart2(input),
-                _ => throw new ArgumentException("Wrong part number - only 1 or 2 allowed")
+                _ => "Wrong part number - only 1 or 2 allowed"
             };
-        }
+
         private static string SolvePart1(List<string> input)
         {
-            return "";
+            int pos = 50;
+            int times_it_points_to_Zero = 0;
+
+            foreach (var rotation in input)
+            {
+                var rotationDir = rotation[0];
+                var rotationAmount = int.Parse(rotation[1..]);
+
+                pos = pos + (rotationDir == 'L' ? (-1 * rotationAmount) : rotationAmount);
+                pos = MathHelper.Modulo(pos, 100);
+
+                if (pos == 0)
+                    times_it_points_to_Zero++;
+            }
+            return times_it_points_to_Zero.ToString();
         }
+
         private static string SolvePart2(List<string> input)
         {
             return "";
