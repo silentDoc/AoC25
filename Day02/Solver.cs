1e6de4dde65c7de2c4bd54537ed5e7ee498143bd
silentDoc <ramon78t@yahoo.es>
2025-12-02 23:58:37 +0100

    Day 2 - Part 1 Completed

--- Day02/Solver.cs
+++ Day02/Solver.cs
@@ -9,8 +9,32 @@
                 2 => SolvePart2(input),
                 _ => "Wrong part number - only 1 or 2 allowed"
             };
+
+        private static IEnumerable<(long, long)> ParsePairs(List<string> input)
+        {
+            var pairs = input[0].Split(',');
+            foreach (var pair in pairs)
+            {
+                var parts = pair.Split('-');
+                yield return (long.Parse(parts[0]), long.Parse(parts[1]));
+            }
+        }
+
+        private static bool HasRepeats(long number)
+        {
+            string str = number.ToString();
+            return str.Length % 2 == 0 && str[..(str.Length / 2)] == str[(str.Length / 2)..];
+        }
+
         private static string SolvePart1(List<string> input)
-            => "Not implemented";
+        { 
+            long sum = 0;
+            foreach (var (first, second) in ParsePairs(input))
+                for (long i = first; i <= second; i++)
+                    if(HasRepeats(i))
+                        sum += i;
+            return sum.ToString();
+        }
 
         private static string SolvePart2(List<string> input)
             => "Not implemented";
