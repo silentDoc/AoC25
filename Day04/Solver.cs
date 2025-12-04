22ecf3d7b02d0472514d5f9bd8e14e161b583ab0
silentDoc <ramon78t@yahoo.es>
2025-12-04 08:31:01 +0100

    Day 4 - Parts 1 and 2 completed.

---
+++ Day04/Solver.cs
@@ -0,0 +1,51 @@
+﻿using AoC25.Common;
+
+namespace AoC25.Day04
+{
+    internal class Solver
+    {
+        static Dictionary<Coord2D, char> grid = new();
+
+        public static string Solve(List<string> input, int part)
+        { 
+            ParseGrid(input);
+            return part == 1 ? SolvePart1(input) : SolvePart2(input);
+        }
+
+        private static void ParseGrid(List<string> input)
+        {
+            Dictionary<Coord2D, char> retVal = new();
+            foreach (var (row, line) in input.Index())
+                foreach(var (col, ch) in line.Index())
+                    grid[new Coord2D(col, row)] = ch;
+        }
+
+        private static string SolvePart1(List<string> input)
+        {
+            var sum = 0;
+            foreach(var key in grid.Keys.Where(x => grid[x]=='@'))
+                sum += key.GetNeighbors8().Where(n => grid.ContainsKey(n)).Count(x => grid[x] == '@') < 4 ? 1 : 0;
+            return sum.ToString();
+        }
+
+        private static string SolvePart2(List<string> input)
+        {
+            List<Coord2D> toRemove = new();
+            bool someRemoved = true;
+            int removed = 0;
+
+            while (someRemoved)
+            {
+                foreach (var key in grid.Keys.Where(x => grid[x] == '@'))
+                    if (key.GetNeighbors8().Where(n => grid.ContainsKey(n)).Count(x => grid[x] == '@') < 4)
+                        toRemove.Add(key);
+                
+                removed += toRemove.Count;
+                someRemoved = toRemove.Count > 0;
+                toRemove.ForEach(x => grid[x] = '.');
+                toRemove.Clear();
+            }
+            return removed.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AoC25
     {
         static void Main(string[] args)
         {
-            int day = 3;
+            int day = 4;
             int part = 2;
             bool test = false;
             int testNum = 0;    // Used only to allow multiple test files per day (e.g., _test1, _test2, etc.)
@@ -24,6 +24,7 @@ namespace AoC25
                 1 => Day01.Solver.Solve(inputLines, part),
                 2 => Day02.Solver.Solve(inputLines, part),
                 3 => Day03.Solver.Solve(inputLines, part),
+                4 => Day04.Solver.Solve(inputLines, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
