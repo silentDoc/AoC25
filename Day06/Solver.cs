366cdf0f5657879db7b4ec97083df4cdaeb3bc25
silentDoc <ramon78t@yahoo.es>
2025-12-07 22:36:09 +0100

    Day 6 - Part 1 Completed

---
+++ Day06/Solver.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC25.Day06
+{
+    internal class Solver
+    {
+        static List<List<long>> rows = new();
+        static List<char> ops = new();
+
+
+        public static string Solve(List<string> lines, int part = 1)
+        {
+            ParseInput(lines);
+            return part == 1 ? SolvePart1() : SolvePart2();
+        }
+
+        static void ParseInput(List<string> lines)
+        {
+            foreach (var line in lines[..^1])
+                rows.Add(line.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => long.Parse(x)).ToList());
+            ops = lines[^1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(x => x[0]).ToList();
+        }
+
+        static string SolvePart1()
+        {
+            var els = Enumerable.Range(0, ops.Count).ToList();
+            List<long> results = els.Select(col => ops[col] == '+' ? rows.Sum(row => row[col])
+                                                                   : rows.Aggregate(1, (long acc, List<long> row) => acc * row[col]))
+                                    .ToList();
+            return results.Sum().ToString();
+        }
+
+        static string SolvePart2()
+            => "Part 2 solution";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace AoC25
     {
         static void Main(string[] args)
         {
-            int day = 5;
-            int part = 2;
+            int day = 6;
+            int part = 1;
             bool test = false;
             int testNum = 0;    // Used only to allow multiple test files per day (e.g., _test1, _test2, etc.)
 
@@ -26,6 +26,7 @@ namespace AoC25
                 3 => Day03.Solver.Solve(inputLines, part),
                 4 => Day04.Solver.Solve(inputLines, part),
                 5 => Day05.Solver.Solve(inputLines, part),
+                6 => Day06.Solver.Solve(inputLines, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
