086a2b9b28185400ddf88ebe46b3a856ed722ba0
silentDoc <ramon78t@yahoo.es>
2025-12-08 00:47:24 +0100

    Day 7 - Part 2 Completed

--- Day07/Solver.cs
+++ Day07/Solver.cs
@@ -3,16 +3,21 @@
     internal class Solver
     {
         public static string Solve(List<string> lines, int part = 1)
-            => part == 1 ? SolvePart1(lines) : SolvePart2(lines);
+            => RunGrid(lines, part);
 
-        private static string SolvePart1(List<string> lines)
+        private static string RunGrid(List<string> lines, int part)
         {
+            int splits = 0; 
+
             HashSet<int> tachyonCols = new();
             tachyonCols.Add(lines[0].IndexOf('S'));
-            int splits = 0;
+            long[] timelines = new long[lines[0].Length];
+            timelines[lines[0].IndexOf('S')] = 1;
 
             for (int row = 1; row < lines.Count; row++)
             {
+                long[] next = new long[lines[0].Length];
+
                 var splitBeams = tachyonCols.Where(c => lines[row][c] == '^').ToHashSet();
                 tachyonCols.RemoveWhere(x => splitBeams.Contains(x));
                 splits += splitBeams.Count;
@@ -20,11 +25,20 @@
                 var splitRight = splitBeams.Select(c => c + 1).ToHashSet();
                 tachyonCols.UnionWith(splitLeft);
                 tachyonCols.UnionWith(splitRight);
-            }
-            return splits.ToString();
-        }
 
-        private static string SolvePart2(List<string> lines)
-            => "Not implemented";
+                for(var col =0; col < lines[0].Length; col++)
+                {
+                    if (lines[row][col] == '.')
+                        next[col] += timelines[col];
+                    else
+                    {
+                        next[col - 1] += timelines[col];
+                        next[col + 1] += timelines[col];
+                    }
+                }
+                timelines = next;
+            }
+            return part == 1 ? splits.ToString() : timelines.Sum().ToString(); 
+        }
     }
 }
