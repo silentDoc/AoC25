4f59e7e348b964e05ecd56e4c5f51c1eec422ce6
silentDoc <ramon78t@yahoo.es>
2025-12-08 12:24:16 +0100

    Day 8 - Part 2 Completed. Solved part 2 by accident trying to speed up part 1 :_)

--- Day08/Solver.cs
+++ Day08/Solver.cs
@@ -2,16 +2,24 @@
 
 namespace AoC25.Day08
 {
+    record PairInfo
+    {
+        public int Item1;
+        public int Item2;
+        public double Distance;
+    }
+
     internal class Solver
     {
         static List<Coord3DL> BoxPositions = new();
-        static Dictionary<(int, int), double> Distances = new();
+        static List<PairInfo> Pairs = new();
         static Dictionary<int, int> BoxInCircuit = new();
+        static Dictionary<(int, int), double> Distances = new();
 
         public static string Solve(List<string> lines, int part = 1)
         {
             ParseInput(lines);
-            return part == 1 ? SolvePart1Alt() : SolvePart2(lines);
+            return part == 1 ? SolvePart1() : SolvePart2();
         }
            
         private static void ParseInput(List<string> lines)
@@ -22,18 +30,30 @@ namespace AoC25.Day08
                 BoxPositions.Add(new Coord3DL(parts[0], parts[1], parts[2]));
             });
 
-            for (int i = 0; i < BoxPositions.Count-1; i++)
+            for (int i = 0; i < BoxPositions.Count - 1; i++)
                 for (int j = i + 1; j < BoxPositions.Count; j++)
                 {
-                    Distances[(i,j)] = BoxPositions[i].DistanceTo(BoxPositions[j]);
-                    Distances[(j,i)] = Distances[(i, j)];
-                }
+                    Pairs.Add(new PairInfo
+                    {
+                        Item1 = i,
+                        Item2 = j,
+                        Distance = BoxPositions[i].DistanceTo(BoxPositions[j])
+                    });
 
+                    Distances[(i, j)] = BoxPositions[i].DistanceTo(BoxPositions[j]);
+                    Distances[(j, i)] = Distances[(i, j)];
+                }
+            
+            // Lookup list to find closest pairs faster
+            // Sort pairs by distance, faster to find closest pairs using "First" later
+            Pairs = Pairs.OrderBy(p => p.Distance).ToList();
+
+            // Labels that indicate which circuit each box belongs to
             for (int i = 0; i < BoxPositions.Count; i++)
                 BoxInCircuit[i] = i;
         }
 
-        private static string SolvePart1Alt()
+        private static string SolvePart1()
         {
             for (int i = 0; i < 1000; i++)
             {
@@ -43,10 +63,10 @@ namespace AoC25.Day08
                 // We find 2 boxes in different circuits - we need to merge circuits
                 if (BoxInCircuit[closestPair.Item1] != BoxInCircuit[closestPair.Item2])
                 {
-                    
+
                     var secondCircuitId = BoxInCircuit[closestPair.Item2];
                     var boxesOfSecond = BoxInCircuit.Keys.Where(x => BoxInCircuit[x] == secondCircuitId);
-                    foreach(var ind in boxesOfSecond)
+                    foreach (var ind in boxesOfSecond)
                         BoxInCircuit[ind] = BoxInCircuit[closestPair.Item1];
                 }
                 Distances.Remove(closestPair);
@@ -57,11 +77,26 @@ namespace AoC25.Day08
                                .Select(g => g.Count())
                                .OrderByDescending(x => x).Take(3)
                                .Aggregate(1, (acc, val) => acc * val)
-                               .ToString(); 
+                               .ToString();
 
         }
 
-        private static string SolvePart2(List<string> lines)
-           => "Not implemented";
+        private static string SolvePart2()
+        {
+            var pairToMerge = Pairs.Where(x => BoxInCircuit[x.Item1] != BoxInCircuit[x.Item2]).FirstOrDefault();
+            long x1 = 0;
+            long x2 = 0;
+            while (pairToMerge!=null)
+            {
+                x1 = BoxPositions[pairToMerge.Item1].x;
+                x2 = BoxPositions[pairToMerge.Item2].x;
+                var firstCircuitId = BoxInCircuit[pairToMerge.Item1];
+                var secondCircuitId = BoxInCircuit[pairToMerge.Item2];
+                var boxesOfSecond = BoxInCircuit.Keys.Where(x => BoxInCircuit[x] == secondCircuitId).ToList();
+                boxesOfSecond.ForEach(x => BoxInCircuit[x] = firstCircuitId);
+                pairToMerge = Pairs.Where(x => BoxInCircuit[x.Item1] != BoxInCircuit[x.Item2]).FirstOrDefault();
+            }
+            return (x1 * x2).ToString();
+        }
     }
 }
